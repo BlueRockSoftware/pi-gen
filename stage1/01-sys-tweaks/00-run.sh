ce023bb83e6baadbb443a554c0e608628b3b23ce
Yash Gandhi <yash@bluerocksoft.com>
2023-10-12 17:27:53 -0700

    modifying config.txt and run.sh to build and compile the kernel from source

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -3,18 +3,62 @@
 install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
-wget https://raw.githubusercontent.com/BlueRockSoftware/rpi-kernel-modules/main/archive/modules-rpi-6.1.21-interludeaudio.tar.gz
-tar -xf modules-rpi-6.1.21-interludeaudio.tar.gz 
-sudo rm -rf modules-rpi-6.1.21-interludeaudio.tar.gz 
-sudo cp modules-rpi-6.1.21-interludeaudio/lib/modules/6.1.21+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko ${ROOTFS_DIR}/lib/modules/6.1.21+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko
-sudo rm ${ROOTFS_DIR}/lib/modules/6.1.21+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko.xz
-sudo cp modules-rpi-6.1.21-interludeaudio/lib/modules/6.1.21-v7+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko ${ROOTFS_DIR}/lib/modules/6.1.21-v7+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko
-sudo rm ${ROOTFS_DIR}/lib/modules/6.1.21-v7+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko.xz
-sudo cp modules-rpi-6.1.21-interludeaudio/lib/modules/6.1.21-v7l+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko ${ROOTFS_DIR}/lib/modules/6.1.21-v7l+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko
-sudo rm ${ROOTFS_DIR}/lib/modules/6.1.21-v7l+/kernel/sound/soc/bcm/snd-soc-rpi-wm8804-soundcard.ko.xz
-sudo cp modules-rpi-6.1.21-interludeaudio/boot/overlays/interludeaudio-analog.dtbo ${ROOTFS_DIR}/boot/overlays/
-sudo cp modules-rpi-6.1.21-interludeaudio/boot/overlays/interludeaudio-digital.dtbo ${ROOTFS_DIR}/boot/overlays/
-sudo rm -rf modules-rpi-6.1.21-interludeaudio
+
+git clone https://github.com/BlueRockSoftware/rpilinux.git
+
+cd rpilinux
+echo "!!!  Build RPi0 kernel and modules  !!!"
+KERNEL=kernel
+make -j 16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig
+make -j 16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install 
+sudo cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
+sudo cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
+sudo cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+
+echo "!!!  RPi0 build done  !!!"
+echo "-------------------------"
+
+echo "!!!  Build RPi3 kernel and modules  !!!"
+KERNEL=kernel7
+make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
+make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install 
+sudo cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
+sudo cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
+sudo cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+
+
+echo "!!!  RPi3 build done  !!!"
+echo "-------------------------"
+
+echo "!!!  Build RPi4 kernel and modules  !!!"
+KERNEL=kernel7l
+make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2711_defconfig
+make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install 
+sudo cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
+sudo cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
+sudo cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+
+echo "!!!  RPi4 build done  !!!"
+echo "-------------------------"
+
+echo "!!!  Build 64-bit kernel and modules  !!!"
+KERNEL=kernel8
+make -j 24  ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+make -j 24  ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
+sudo cp arch/arm64/boot/Image ${ROOTFS_DIR}/boot/$KERNEL.img
+sudo cp arch/arm64/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
+sudo cp arch/arm64/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+
+sudo rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21+
+sudo rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v7+
+sudo rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v7l+
+sudo rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v8+
+
+echo "!!! All builds completed !!!"
 
 on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
