1289f3d380a7e38dc9b23bf03fa3735232758e1c
Stephen Anthony Jones <stephen@bluerocksoft.com>
2023-11-10 11:08:56 -0800

    Using rpilinux v5.15 artifacts instead of building them each time

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -4,66 +4,16 @@ install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
-git clone https://github.com/BlueRockSoftware/rpilinux.git
+rm -rf ${ROOTFS_DIR}/lib/modules/*
 
-cd rpilinux
-echo "!!!  Build RPi0 kernel and modules  !!!"
-KERNEL=kernel
-make -j 16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig
-make -j 16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
-env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
-cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
-cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
-cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+KERNEL_VER=v5.15
+cp -r files/rpilinux/${KERNEL_VER}/arch/arm/lib/modules/* ${ROOTFS_DIR}/lib/modules/.
+cp    files/rpilinux/${KERNEL_VER}/arch/arm/boot/kernel*.img ${ROOTFS_DIR}/boot/.
+cp    files/rpilinux/${KERNEL_VER}/arch/arm/boot/dts/overlays/* ${ROOTFS_DIR}/boot/overlays/.
 
-echo "!!!  RPi0 build done  !!!"
-echo "-------------------------"
-
-echo "!!!  Build RPi3 kernel and modules  !!!"
-KERNEL=kernel7
-make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
-make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
-env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
-cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
-cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
-cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
-
-
-echo "!!!  RPi3 build done  !!!"
-echo "-------------------------"
-
-echo "!!!  Build RPi4 kernel and modules  !!!"
-KERNEL=kernel7l
-make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2711_defconfig
-make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
-env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
-cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
-cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
-cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
-
-echo "!!!  RPi4 build done  !!!"
-echo "-------------------------"
-
-echo "!!!  Build 64-bit kernel and modules  !!!"
-KERNEL=kernel8
-make -j 24  ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
-make -j 24  ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
-env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
-cp arch/arm64/boot/Image ${ROOTFS_DIR}/boot/$KERNEL.img
-cp arch/arm64/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
-cp arch/arm64/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
-
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21+
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v7+
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v7l+
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v8+
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.0-rpi4-rpi-2712
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.0-rpi4-rpi-v8
-
-echo "!!!  64-bit build done  !!!"
-echo "-------------------------"
-
-echo "!!! All builds completed !!!"
+cp -r files/rpilinux/${KERNEL_VER}/arch/arm64/lib/modules/* ${ROOTFS_DIR}/lib/modules/.
+cp    files/rpilinux/${KERNEL_VER}/arch/arm64/boot/kernel*.img ${ROOTFS_DIR}/boot/.
+cp    files/rpilinux/${KERNEL_VER}/arch/arm64/boot/dts/overlays/* ${ROOTFS_DIR}/boot/overlays/.
 
 on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
