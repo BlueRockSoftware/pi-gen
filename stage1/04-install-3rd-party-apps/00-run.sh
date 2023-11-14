2d1e8f5d1ddac9ec36c2c9ef2fa7ecb2a0b301b2
Stephen Anthony Jones <stephen@bluerocksoft.com>
2023-11-14 01:03:45 -0800

    Adding 3rd party apps as part of pi-gen build

--- stage1/04-install-3rd-party-apps/00-run.sh
+++ stage1/04-install-3rd-party-apps/00-run.sh
@@ -1,5 +1,50 @@
 #!/bin/bash -e
 
+on_chroot << EOF
+#
+#  S H A I R P O R T
+
+# Build and Install nqptp
+git clone https://github.com/mikebrady/nqptp.git
+cd nqptp
+autoreconf -fi
+./configure --with-systemd-startup
+make
+make install
+cd ..
+
+# Enable and Start nqptp
+systemctl enable nqptp
+
+# Build and Install Shairport
+git clone https://github.com/mikebrady/shairport-sync.git
+cd shairport-sync
+autoreconf -fi
+./configure --sysconfdir=/etc --with-pa \
+    --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-airplay-2
+make
+make install
+cd ..
+
+
+#
+#  R A S P O T I F Y
+curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
+
+# Set card for Raspotify to use
+sed -i "s\#LIBRESPOT_DEVICE=\"default\"\LIBRESPOT_DEVICE=\"pulse\"\g" /etc/raspotify/conf
+
+# set Raspotify to use system wide PulseAudio
+sed -i '/\[Service\]/a\User=pulse' /lib/systemd/system/raspotify.service
+
+
+#
+#  R O O N  B R I D G E
+curl -O https://download.roonlabs.net/builds/roonbridge-installer-linuxarmv7hf.sh
+chmod +x roonbridge-installer-linuxarmv7hf.sh
+yes Y | ./roonbridge-installer-linuxarmv7hf.sh
+EOF
+
 # Install script that runs on first boot to install 3rd party apps
 install -m 755 files/install-3rd-party-apps "${ROOTFS_DIR}/usr/local/sbin/install-3rd-party-apps"
 
