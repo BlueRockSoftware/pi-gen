89c1b0ba7582bfff3fd2d85d174c2ba1a2a51d57
Stephen Anthony Jones <stephen@bluerocksoft.com>
2023-11-13 17:03:36 -0800

    Adding optional mirrors

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -7,4 +7,5 @@ fi
 
 if [ ! -d "${ROOTFS_DIR}" ] || [ "${USE_QCOW2}" = "1" ]; then
 	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
+	# bootstrap ${RELEASE} "${ROOTFS_DIR}" http://mirrors.gigenet.com/raspbian/raspbian/ # Optional mirror if http://raspbian.raspberrypi.org/raspbian/ doesn't work
 fi
