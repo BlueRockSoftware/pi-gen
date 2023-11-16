dca94e58c342fb200a65e9fbe2f552703235eea7
Stephen Anthony Jones <stephen@bluerocksoft.com>
2023-11-16 15:31:18 -0800

    WIP: Removed need for first boot script

--- stage1/04-install-3rd-party-apps/00-run.sh
+++ stage1/04-install-3rd-party-apps/00-run.sh
@@ -45,12 +45,6 @@ chmod +x roonbridge-installer-linuxarmv7hf.sh
 yes Y | ./roonbridge-installer-linuxarmv7hf.sh
 EOF
 
-# Install script that runs on first boot to install 3rd party apps
-install -m 755 files/install-3rd-party-apps "${ROOTFS_DIR}/usr/local/sbin/install-3rd-party-apps"
-
-# Install systemd service to run install-3rd-party-apps on first boot
-install -m 644 files/install-3rd-party-apps.service "${ROOTFS_DIR}/etc/systemd/system/install-3rd-party-apps.service"
-ln -svf "/etc/systemd/system/install-3rd-party-apps.service" "$ROOTFS_DIR/etc/systemd/system/multi-user.target.wants/install-3rd-party-apps.service"
 
 # Install systemd service to run pulseaudio as a system service
 install -m 644 files/pulseaudio.service "${ROOTFS_DIR}/etc/systemd/system/pulseaudio.service"
--- stage4/05-print-support/01-run.sh
+++ stage4/05-print-support/01-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+sed -i 's/; default-sample-rate = 44100/default-sample-rate = 48000/' ${ROOTFS_DIR}/etc/pulse/daemon.conf
+touch ${ROOTFS_DIR}/etc/pulse/plusultra
+
 on_chroot <<EOF
 adduser "$FIRST_USER_NAME" lpadmin
 EOF
