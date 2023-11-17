45e6dfe065a95dcb23472deb0cf7662601bfd6f2
Stephen Anthony Jones <stephen@bluerocksoft.com>
2023-11-17 11:39:50 -0800

    WIP: Removed need for first boot script - part 2

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -5,3 +5,7 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/share"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/share/applications"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/share/desktop-directories"
+
+#Set pulseaudio default sample rate to 48000
+sed -i 's/; default-sample-rate = 44100/default-sample-rate = 48000/' ${ROOTFS_DIR}/etc/pulse/daemon.conf
+touch ${ROOTFS_DIR}/etc/pulse/plusultra
--- stage4/05-print-support/01-run.sh
+++ stage4/05-print-support/01-run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-sed -i 's/; default-sample-rate = 44100/default-sample-rate = 48000/' ${ROOTFS_DIR}/etc/pulse/daemon.conf
-touch ${ROOTFS_DIR}/etc/pulse/plusultra
-
 on_chroot <<EOF
 adduser "$FIRST_USER_NAME" lpadmin
 EOF
