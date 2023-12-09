15d1f900fb0fc1b45a60814b734f3873ee6331ab
Stephen Anthony Jones <stephen@bluerocksoft.com>
2023-12-08 20:55:00 -0800

    Download kernel artifacts instead of building

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -4,66 +4,73 @@ install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
-git clone https://github.com/BlueRockSoftware/rpilinux.git
+rm -rf ${ROOTFS_DIR}/lib/modules/*
 
-cd rpilinux
-echo "!!!  Build RPi0 kernel and modules  !!!"
-KERNEL=kernel
-make -j 16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig
-make -j 16 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
-env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
-cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
-cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
-cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+# Download and install kernel artifacts
+####################
+REPO_OWNER="BlueRockSoftware"
+REPO_NAME="rpilinux"
+BRANCH_NAME="rpi-6.1.21"
+WORKFLOW="kernel-build.yml"
 
-echo "!!!  RPi0 build done  !!!"
-echo "-------------------------"
+# ACCESS_TOKEN="" # Set via environment variable
 
-echo "!!!  Build RPi3 kernel and modules  !!!"
-KERNEL=kernel7
-make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
-make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
-env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
-cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
-cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
-cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+# GitHub API endpoint to get the latest workflow run on the specified branch
+WORKFLOW_RUN_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/kernel-build.yml/runs?branch=${BRANCH_NAME}"
+echo "WORKFLOW_RUN_URL: ${WORKFLOW_RUN_URL}"
 
+# Get the latest workflow run ID
+WORKFLOW_RUN_ID=$(curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${ACCESS_TOKEN}" ${WORKFLOW_RUN_URL} | jq -r '.workflow_runs[0].id')
+echo "WORKFLOW_RUN_ID: ${WORKFLOW_RUN_ID}"
 
-echo "!!!  RPi3 build done  !!!"
-echo "-------------------------"
+# GitHub API endpoint to get the artifacts for the latest workflow run
+ARTIFACTS_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runs/${WORKFLOW_RUN_ID}/artifacts"
+echo "ARTIFACTS_URL: ${ARTIFACTS_URL}"
 
-echo "!!!  Build RPi4 kernel and modules  !!!"
-KERNEL=kernel7l
-make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2711_defconfig
-make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
-env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
-cp arch/arm/boot/zImage ${ROOTFS_DIR}/boot/$KERNEL.img
-cp arch/arm/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
-cp arch/arm/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+# Save the current IFS value
+OLD_IFS=$IFS
+# Set IFS to newline
+IFS=$'\n'
+# Tokenize the input string into an array
+artifacts=($(curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${ACCESS_TOKEN}" ${ARTIFACTS_URL} | jq -r '.artifacts[] | "\(.id) \(.name)"'))
+# Restore the original IFS value
+IFS=$OLD_IFS
 
-echo "!!!  RPi4 build done  !!!"
-echo "-------------------------"
+# Specify the list of artifact names to download
+ARTIFACTS_TO_DOWNLOAD=("bcmrpi_build" "bcm2709_build" "bcm2711_build" "bcm2711_arm64_build")
 
-echo "!!!  Build 64-bit kernel and modules  !!!"
-KERNEL=kernel8
-make -j 24  ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
-make -j 24  ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
-env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${ROOTFS_DIR}/ modules_install
-cp arch/arm64/boot/Image ${ROOTFS_DIR}/boot/$KERNEL.img
-cp arch/arm64/boot/dts/overlays/*.dtb* ${ROOTFS_DIR}/boot/overlays
-cp arch/arm64/boot/dts/overlays/README ${ROOTFS_DIR}/boot/overlays
+# Loop through each artifact
+for artifact in "${artifacts[@]}"; do
+  # Extract artifact ID and name
+  ARTIFACT_ID=$(echo $artifact | cut -d' ' -f1)
+  ARTIFACT_NAME=$(echo $artifact | cut -d' ' -f2-)
 
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21+
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v7+
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v7l+
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.21-v8+
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.0-rpi4-rpi-2712
-rm -rf ${ROOTFS_DIR}/lib/modules/6.1.0-rpi4-rpi-v8
+  # Check if the artifact is in the list of artifacts to download
+  if [[ " ${ARTIFACTS_TO_DOWNLOAD[@]} " =~ " ${ARTIFACT_NAME} " ]]; then
+    echo ""
+    echo "ARTIFACT_ID: ${ARTIFACT_ID}"
+    echo "ARTIFACT_NAME: ${ARTIFACT_NAME}"
 
-echo "!!!  64-bit build done  !!!"
-echo "-------------------------"
+    # GitHub API endpoint to download the artifact
+    DOWNLOAD_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/artifacts/${ARTIFACT_ID}/zip"
 
-echo "!!! All builds completed !!!"
+    # Download the artifact
+    curl -L -o ${ARTIFACT_NAME}.zip -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${ACCESS_TOKEN}" ${DOWNLOAD_URL}
+
+	# Extract the artifact
+    unzip -o ${ARTIFACT_NAME}.zip
+    tar -xvf ${ARTIFACT_NAME}.tar
+
+    echo "Downloaded artifact: ${ARTIFACT_NAME}.zip"
+  fi
+done
+
+echo "Installing kernel artifacts..."
+mv boot/*.dtb ${ROOTFS_DIR}/boot/.
+mv boot/*.img ${ROOTFS_DIR}/boot/.
+mv boot/overlays/* ${ROOTFS_DIR}/boot/overlays/.
+mv lib/modules/* ${ROOTFS_DIR}/lib/modules/.
+####################
 
 on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
